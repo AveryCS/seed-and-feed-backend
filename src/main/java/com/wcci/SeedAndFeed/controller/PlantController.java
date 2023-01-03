7af218171bd9a4fd07eff5c40ab4668cfe19e671
AveryCS <Averydcs@gmail.com>
2023-01-03 12:53:27 -0500

    adding CrossOrigin to the plant controller

--- src/main/java/com/wcci/SeedAndFeed/controller/PlantController.java
+++ src/main/java/com/wcci/SeedAndFeed/controller/PlantController.java
@@ -3,14 +3,12 @@ package com.wcci.SeedAndFeed.controller;
 import com.wcci.SeedAndFeed.entities.Plant;
 import com.wcci.SeedAndFeed.repos.PlantRepository;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.ArrayList;
 
 @RestController
+@CrossOrigin
 public class PlantController {
    private PlantRepository plantRepo;
 
